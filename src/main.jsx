04c62b38e406209d493de4c34a875d1bb72b4ec9
BViscay <brunoviscay@gmail.com>
2025-03-18 17:41:04 -0300

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,14 @@ import {HeroUIProvider} from "@heroui/react";
 import {BrowserRouter} from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
-import ScrollToTop from "./Components/ScrollToTop.jsx";
+
 
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StrictMode>
       <HeroUIProvider>
-        <ScrollToTop />
+       
         <App />
       </HeroUIProvider>
     </StrictMode>
