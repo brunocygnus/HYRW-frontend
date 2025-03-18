73d418757f3f59f9136891f7f418df8c163c447c
BViscay <brunoviscay@gmail.com>
2025-03-18 17:45:51 -0300

    changes1

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import NavBar from "./Components/NavBar";
 import Landing from "./views/Landing";
 import Footer from "./Components/Footer";
 
+
 function App() {
   return (
     <div className='w-full flex flex-col items-center h-autp justify-center'>
